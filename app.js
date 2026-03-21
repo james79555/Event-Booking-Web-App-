81f8ef3036991744651bdbe6f953e35bf07fb308
james79555 <james241201@yahoo.co.uk>
2026-03-21 14:05:16 +0000

    chore: installed express and started setting up express server in app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const eventRoutes = require('./routes/eventRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/events', eventRoutes);
+app.use('/users', userRoutes);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
