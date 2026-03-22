becc3f80c49af32745a6360e143bbcde3f676aba
james79555 <james241201@yahoo.co.uk>
2026-03-22 11:10:47 +0000

    feat: connecting node to postgres database - .env and db.js files created

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const {Pool} = require('pg'); 
+require('dotenv').config(); 
+
+const pool = new Pool({
+    user: process.env.DB_USER, 
+    host: process.env.DB_HOST, 
+    port: process.env.DB_PORT, 
+    database: process.env.DB_NAME
+}); 
+
+pool.connect()
+    .then( () => console.log("Successfully connected to PostgreSQL!"))
+    .catch(err => console.error("Database connection error", err.stack)); 
+
+module.exports = pool; 
