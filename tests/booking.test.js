03ed3624659417f0ddf87c4d1ce9cd035e59904d
james79555 <james241201@yahoo.co.uk>
2026-03-24 23:20:48 +0000

    test:ensure UI page displays users booking correctly

--- tests/booking.test.js
+++ tests/booking.test.js
@@ -55,4 +55,15 @@ describe('GET /bookings - Show User Bookings', () => {
     });
 });
 
+describe('GET /bookings route - View UI Tests', () => {
+    it("should return an HTML webpage containing the user's tickets", async () => {
+        const response = await request(app).get("/bookings?userId=1");
+
+        expect(response.status).toBe(200); 
+        expect(response.headers["content-type"]).toContain("text/html");
+        expect(response.text).toContain("<h1>My Bookings</h1>"); 
+        expect(response.text).toContain("Ticket Quantity:"); 
+    });
+});
+
 module.exports = app; 
