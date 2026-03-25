2ecccc9b6c35f4f8a6a0c3d892e13621abdedc15
james79555 <james241201@yahoo.co.uk>
2026-03-25 23:53:02 +0000

    test: check event details and booking form display correctly

---
+++ tests/event.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest'); 
+const app = require('../app')
+
+describe('GET /eevnts/:id - View Eveent Details', () =>{
+    it('should display the event details and a booking form', async () => {
+        const response = await request(app).get(/events/1);
+
+        expect(response.status).toBe(200);
+        expect(response.headers["content-type"]).toContain("text/html");
+        expect(response.text).toContain('action="/bookings');
+        expect(response.text).toContain('method="POST"');
+        expect(response.text).toContain('name="ticketQuantity"');
+    });
+});
