1805b2b5e3d78795c82ed0451c69aebb4ed41f4e
james79555 <james241201@yahoo.co.uk>
2026-03-28 08:34:34 +0000

    tests: return successfully if user is created and return accordingly if fields are missing

---
+++ tests/user.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest'); 
+const app = require('../app');
+
+describe('POST /register - User Registration', () => {
+    it('should successfully register a new user and return a 201 status', async () => {
+        const timestamp = Date.now()
+        const testEmail = "user" + timestamp + "@example.com"
+
+        const response = await request(app)
+            .post('/register')
+            .send({
+                name: "Test User",
+                email: testEmail,
+                password: "Secure?1289"
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.text).toContain('Registration successful');
+    });
+
+    it('should return a 400 status if required fields are missing', async () => {
+        const response = await request(app)
+            .post('/register')
+            .send({
+                name: "Incomplete user",
+                email: "incomplete@example.com"
+            });
+
+            expect(response.status).toBe(400);
+            expect(response.text).toContain('All fields are required');
+    })
+})
